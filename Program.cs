58e0af079bbeefadc24494b6a93e816c21f9a17e
MaKarastY <71918286+makarasty@users.noreply.github.com>
2024-02-19 20:19:09 +0200

    1.0.0b

--- Program.cs
+++ Program.cs
@@ -1,7 +1,18 @@
-﻿public class Program()
+﻿namespace MyOverwrite;
+
+public class Program
 {
 	public static void Main(string[] args)
 	{
 		System.Console.WriteLine("Hello world ♥");
+
+		MyPoint point1 = new MyPoint(1000, 28);
+		MyPoint point2 = new MyPoint(7, 228);
+
+		MyPoint point3 = point1 + point2;
+		Console.WriteLine($"{point3.X}, {point3.Y}");
+
+		MyPoint point4 = point1 - point2;
+		Console.WriteLine($"{point4.X}, {point4.Y}");
 	}
 }
